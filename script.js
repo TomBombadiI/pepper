184f3908f896436f964eb6638534b4b19ccb0fe9
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2024-12-11 13:52:26 +0700

    end

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    $('.burger-open').click(function () {
+        $('.burger-open').toggleClass('active');
+        $('.menu-mobile').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+    $('.menu-mobile__link').click(() => {
+        $('.menu-mobile').removeClass('active');
+        $('.burger-open').removeClass('active');
+        $('body').removeClass('lock');
+    })
+
+    const swiper = new Swiper('.banner', {
+        slidesPerView: 1,
+        loop: true,
+        autoplay: {
+            delay: 8000,
+        },
+        cssMode: true,
+    });
+
+    $("a[href^='#']").click(function() {
+        $('html, body').animate({
+            scrollTop: $("" + $(this).attr("href") + "").offset().top
+        }, 700);
+    });
+});
